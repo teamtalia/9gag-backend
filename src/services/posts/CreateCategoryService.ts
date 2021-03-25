95abfccb568cdba844618337905e6d7ba6a66266
MateusAlvesP <mateusp2000@hotmail.com>
2021-03-25 16:08:52 -0300

    feat: developed function to add categories

---
+++ src/services/posts/CreateCategoryService.ts
@@ -0,0 +1,37 @@
+import { getRepository } from 'typeorm';
+import slugify from 'slugify';
+import ServiceError from '../../util/ServiceError';
+import Category from '../../models/Category';
+
+interface Request {
+  name: string;
+}
+
+class CreateTagService {
+  public async execute({ name }: Request): Promise<Category> {
+    const categoriesRepository = getRepository(Category);
+
+    const createdAt = new Date();
+    const updatedAt = new Date();
+
+    try {
+      const categoryData = await categoriesRepository.create({
+        name,
+        slug: slugify(name),
+        createdAt,
+        updatedAt,
+      });
+      const category = await categoriesRepository.save(categoryData);
+      if (category) {
+        return await categoriesRepository.findOne({
+          where: { id: category.id },
+        });
+      }
+      throw new ServiceError(`error on retrive new tag from database.`);
+    } catch (err) {
+      throw new ServiceError(`error on create tag: ${err}`);
+    }
+  }
+}
+
+export default CreateTagService;

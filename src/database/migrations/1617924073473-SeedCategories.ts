9f36a7d0b6775246111c019ff013577e730f7839
Thales de Oliveira <thalesoliveira200@gmail.com>
2021-04-25 10:30:20 -0300

    refactor: remove try/catch for x times

--- src/database/migrations/1617924073473-SeedCategories.ts
+++ src/database/migrations/1617924073473-SeedCategories.ts
@@ -22,22 +22,6 @@ export class SeedCategories1617924073473 implements MigrationInterface {
   ];
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    let times = 0;
-    let categoryRepository;
-    while (true) {
-      try {
-        categoryRepository = queryRunner.manager.getRepository(Category);
-        break;
-      } catch (e) {
-        times += 1;
-        if (times === 3) {
-          throw new Error('Cannot get category repository');
-        }
-        // eslint-disable-next-line no-await-in-loop
-        await new Promise(resolve => setTimeout(() => resolve(), 3000));
-      }
-    }
-
     await Promise.all(
       this.payload.map(category =>
         queryRunner.manager

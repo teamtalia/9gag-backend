6a9630115fc69706531b7a056446ca9c3bb2de4b
Thales de Oliveira <thalesoliveira200@gmail.com>
2021-04-25 10:28:43 -0300

    refactor: refactor seed categories

--- src/database/migrations/1617924073473-SeedCategories.ts
+++ src/database/migrations/1617924073473-SeedCategories.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-constant-condition */
 import slugify from 'slugify';
 import { MigrationInterface, QueryRunner } from 'typeorm';
+import { uniqueId } from 'lodash';
 import Category from '../../models/Category';
 
 export class SeedCategories1617924073473 implements MigrationInterface {
@@ -37,13 +38,21 @@ export class SeedCategories1617924073473 implements MigrationInterface {
       }
     }
 
-    await categoryRepository.save(
-      this.payload.map(name => ({
-        name,
-        slug: slugify(name.toLowerCase()),
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      })),
+    await Promise.all(
+      this.payload.map(category =>
+        queryRunner.manager
+          .createQueryBuilder()
+          .insert()
+          .into('categories')
+          .values({
+            id: uniqueId(),
+            name: category,
+            slug: slugify(category.toLowerCase()),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          })
+          .execute(),
+      ),
     );
   }
 
